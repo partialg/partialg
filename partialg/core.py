fc91e59e1ee0ee232691da1ddcb7e41c5e3ed806
Lima <deaq54989@hbku.edu.qa>
2025-08-07 11:05:34 +0300

    Update core.py

--- partialg/core.py
+++ partialg/core.py
@@ -1,3 +1,36 @@
+# START OF LICENSE DECLARATION.
+#
+# CC BY-NC-ND 4.0 License
+#
+# (Attribution-NonCommercial-NoDerivatives 4.0 International)
+#
+# Copyright (c) 2025 Dennis Lima
+#
+# YOU ARE FREE TO share — copy and redistribute the material in any medium 
+# or format. The licensor cannot revoke these freedoms as long as you follow the 
+# license terms.
+#
+# UNDER THE FOLLOWING TERMS:
+#     (i) Attribution — You must give appropriate credit, provide a link to the 
+# license, and indicate if changes were made. You may do so in any reasonable 
+# manner, but not in any way that suggests the licensor endorses you or your 
+# use.
+#     (ii) NonCommercial — You may not use the material for commercial purposes .
+#     (iii) NoDerivatives — If you remix, transform, or build upon the material, you 
+# may not distribute the modified material.
+#     (iv) No additional restrictions — You may not apply legal terms or technological 
+# measures that legally restrict others from doing anything the license permits.
+#
+# Notices:
+#     (i) You do not have to comply with the license for elements of the material in the 
+# public domain or where your use is permitted by an applicable exception or 
+# limitation.
+#     (ii) No warranties are given. The license may not give you all of the permissions 
+# necessary for your intended use. For example, other rights such as publicity, 
+# privacy, or moral rights may limit how you use the material.
+#     (iii) View this license online at https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en.
+#
+# END OF LICENSE DECLARATION.
 
 def pinv(M, *args, **kwargs):
     ''' Partial inversion algorithm
@@ -40,5 +73,6 @@ def sbd(a, **kwargs):
         return sbd_eigenvaluey(a)
     else:
         raise Warning("ABORTED. Only sparse, dense or symbolic are supported.")
+
 
 #
