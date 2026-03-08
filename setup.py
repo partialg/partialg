aaed0b5886cd729d4140199e55bbcdc03584af3d
Lima <dal041795@gmail.com>
2026-03-08 18:48:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
 
+long_description = ""
+try:
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    pass
+
 setup(
     name="partialg",                 
     version="0.0.1",                  
     author="Dennis Lima",
     author_email="deaq54989@hbku.edu.qa",
     description="Partial implementation of matrix inversion, diagonalization and its applications.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/partialg/partialg",
-    packages=find_packages(),         # Automatically find all sub-packages
+    packages=find_packages(where="partialg"),
+    package_dir={"": "partialg"},
+    license="CC BY-NC-ND 4.0",
     install_requires=[                # Dependencies
         "numpy>=2.0.2",
         "scipy>=1.16.1",
@@ -20,7 +29,7 @@ setup(
     python_requires=">=3.9",
     classifiers=[                     # Metadata for PyPI
         "Programming Language :: Python :: 3",
-        "License :: :: CC BY-NC-ND 4.0 License",
+        "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
 )
