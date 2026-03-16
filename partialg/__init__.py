759ab93b8481584a048460f93f273a7a83ea68ab
Lima <dal041795@gmail.com>
2026-03-16 21:35:40 +0300

    Update __init__.py

--- partialg/__init__.py
+++ partialg/__init__.py
@@ -1,7 +1,7 @@
-from .core import pinv, inv, peigval, help
+from .core import pinv, inv, peigval, help, help_me_go_vegan
 from .experimental import dok as experimental_dok
 
-__all__ = ["pinv", "inv", "peigval", "help", "experimental"]
+__all__ = ["pinv", "inv", "peigval", "help", "help_me_go_vegan", "experimental"]
 
 
 
