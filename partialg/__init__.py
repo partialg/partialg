1b26cde2ce49b94a6dfaf93b3c613c7c056f6d18
Lima <dal041795@gmail.com>
2026-07-29 21:41:44 +0100

    Update __all__ to include dense, sparse, and symbolic
    Added dense, sparse, and symbolic modules to __all__.

--- partialg/__init__.py
+++ partialg/__init__.py
@@ -1,7 +1,8 @@
 from .core import pinv, inv, peigval, help, help_me_go_vegan
+from . import dense, sparse, symbolic
 from .experimental import dok
 
-__all__ = ["pinv", "inv", "peigval", "help", "help_me_go_vegan", "experimental", "dok"]
+__all__ = ["pinv", "inv", "peigval", "help", "help_me_go_vegan", "experimental", "dok", "dense", "sparse", "symbolic"]
 
 
 
