f158f76c6bea54c7e05343e01f3ea05dbba69fa0
Lima <dal041795@gmail.com>
2026-07-29 21:40:40 +0100

    Add 'dok' to __all__ in __init__.py

--- partialg/__init__.py
+++ partialg/__init__.py
@@ -1,7 +1,7 @@
 from .core import pinv, inv, peigval, help, help_me_go_vegan
-from .experimental import dok as experimental_dok
+from .experimental import dok
 
-__all__ = ["pinv", "inv", "peigval", "help", "help_me_go_vegan", "experimental"]
+__all__ = ["pinv", "inv", "peigval", "help", "help_me_go_vegan", "experimental", "dok"]
 
 
 
