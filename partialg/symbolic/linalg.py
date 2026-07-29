d8b7590aa637ddd949179e4f1fef70adf2fe1b4e
Lima <dal041795@gmail.com>
2026-07-29 22:48:45 +0100

    Implement rotation matrices and madjoint function
    Added functions for stretched rotation matrices and memory-space conjugation.

--- partialg/symbolic/linalg.py
+++ partialg/symbolic/linalg.py
@@ -4,14 +4,31 @@ Provides matrix expressions involving khaguna.
 Note:
 In memory space (using khaguna as zero), 
 U**-1 @ M @ U, where U is the eigenvector matrix of M, 
-can always be used to diagonalize M, but if M is not 
-diagonalizable, "diagonalization" loses meaning as a 
-name, as the matrix entries will be "spread" over the 
-matrix rather than concentrated along a diagonal. 
+can always be used to diagonalize M, even if U is not invertible
+in the traditional sense. If M is not diagonalizable, 
+"diagonalization" loses meaning as a name, as the 
+matrix entries will be "spread" over the matrix rather 
+than concentrated along a diagonal. 
 """
 
 from .math import o
-from sympy import Matrix, sqrt
+from sympy import Matrix, sqrt, sin, cos
+
+def rotation_o(th, khaguna=o):
+    "Stretched rotation matrix with khaguna determinant."
+    return Matrix([[khaguna*cos(th), -khaguna*sin(th)],[sin(th), cos(th)]] )
+
+def rotation_oo(th, khaguna=o):
+    "Stretched rotation matrix with khahara determinant."
+    return Matrix([[cos(th), (1/khaguna)*sin(th)],[-sin(th), (1/khaguna)*cos(th)]] )
+
+def rotation_1(th, khaguna=o):
+    "Stretched rotation matrix with unit determinant."
+    return Matrix([[khaguna*cos(th), -khaguna*sin(th)],[(1/khaguna)*sin(th), (1/khaguna)*cos(th)]] )
+
+def madjoint(m, khaguna=o):
+    "Memory-space-conjugate with complex-conjugate and transpose of matrix m."
+    return m.conjugate().T.subs(khaguna, 1/khaguna)
 
 def eigenvalues2(m, khaguna=o):
     "Eigenvalues for symbolic matrix m of shape (2,2)."
